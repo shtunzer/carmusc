af2430b03bf1f4b440730dd5252baa0d2e9ac3f4
Denis <130508415+shtunzer@users.noreply.github.com>
2023-04-21 13:06:56 +0300

    Update service.js

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,31 +1,35 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-200px";
-  }
-  prevScrollpos = currentScrollPos;}
-
-  function scrollTo(element) {
-    window.scroll({
-        left: 0,
-        top: element.offsetTop,
-        behavior: 'smooth'
-    })
-}
-
+function scrollTo(element) {
+  window.scroll({
+      left: 0,
+      top: element.offsetTop,
+      behavior: 'smooth'
+  })
+}  
 var button4 = document.querySelector('.scrollTo4');
 var price = document.querySelector('#price');
-
 button4.addEventListener('click' , () => {
-    scrollTo(price);
+  scrollTo(price);
 });
-
 document.getElementById('burgerMenu__btn-open').addEventListener('click', function(){
-    document.getElementById('burgerMenu').classList.add('--show');
+  document.getElementById('burgerMenu').classList.add('--show');
 });
 document.getElementById('burgerMenu__btn-hide').addEventListener('click', function(){
-    document.getElementById('burgerMenu').classList.remove('--show');
+  document.getElementById('burgerMenu').classList.remove('--show');
+});
+
+const advantages__swiper_1 = new Swiper(".advantages__swiperBlock_1", {
+  navigation: {
+      nextEl: ".advantages__swiper-button-next_1",
+      prevEl: ".advantages__swiper-button-prev_1",
+  },
+});
+
+document.querySelector('.advantages__swiper-button-next_1').addEventListener('click', function(){
+  document.querySelector('.advantages__swiper-button-prev_1').classList.remove('--opacity-btn');
+  document.querySelector('.advantages__swiper-button-next_1').classList.add('--opacity-btn');
+});
+
+document.querySelector('.advantages__swiper-button-prev_1').addEventListener('click', function(){
+  document.querySelector('.advantages__swiper-button-next_1').classList.remove('--opacity-btn');
+  document.querySelector('.advantages__swiper-button-prev_1').classList.add('--opacity-btn');
 });
